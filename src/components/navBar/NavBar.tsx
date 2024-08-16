8d3a443c09eea07af5ed9dd2f77bea4427be1018
Yasmin de França Ribeiro <64998808+yasmin-fribeiro@users.noreply.github.com>
2024-08-16 11:19:00 -0300

    Segunda Task com as rotas

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,3 +1,4 @@
+import { Link, useNavigate } from 'react-router-dom'
 import React from 'react'
 
 
@@ -12,8 +13,10 @@ function Navbar() {
             <div className='text-2xl font-bold uppercase'>Farmácia</div>
 
             <div className='flex gap-4'>
+              <Link to='/login' className='hover:underline'>Login</Link>
+              <Link to='/home' className='hover:underline'>Home</Link>
               <div className='hover:underline'>Produtos</div>
-              <div className='hover:underline'>Categoria</div>
+              <div className='hover:underline'>Categorias</div>
               <div className='hover:underline'>Cadastrar categoria</div>
               <div className='hover:underline'>Sair</div>
             </div>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -9,8 +9,11 @@ function Home() {
         <div className="bg-indigo-900 flex justify-center">
           <div className='container grid grid-cols-2 text-white'>
             <div className="flex flex-col gap-4 items-center justify-center py-4">
-              <h2 className='text-5xl font-bold'>Seja bem vinde!</h2>  
-            
+              <h2 className='text-5xl font-bold'>Seja bem vinde!</h2>
+  
+              <div className="flex justify-around gap-4">
+              
+              </div>
             </div>
   
             <div className="flex justify-center ">
